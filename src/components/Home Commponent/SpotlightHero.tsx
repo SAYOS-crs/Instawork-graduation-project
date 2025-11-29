4ec799ac94f023c805d2f3e52ff79b54ada788b5
Eslam Elawdy <eslam.mohamed.ki123@gmail.com>
2025-11-29 02:13:42 +0200

    Feat(ServiceDetails): Service Details Added

--- src/components/Home Commponent/SpotlightHero.tsx
+++ src/components/Home Commponent/SpotlightHero.tsx
@@ -11,10 +11,10 @@ export default function SpotlightHero() {
     <div className="w-full rounded-b-md flex md:items-center md:justify-center bg-black/90 antialiased bg-grid-white/[0.02] relative overflow-hidden min-h-screen font-poppins">
       <Spotlight />
       <div className="p-4 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0">
-        <h1 className="text-4xl py-3 md:text-7xl font-bold text-center bg-clip-text text-transparent bg-linear-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+        <h1 className="text-4xl  py-3 md:text-7xl font-bold text-center bg-clip-text text-transparent bg-linear-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           {t("header_title")}
         </h1>
-        <p className="mt-4 font-light text-base text-neutral-300 max-w-lg text-center mx-auto">
+        <p className="mt-4  font-light text-base text-neutral-300 max-w-lg text-center mx-auto">
           {t("header_paragraph")}
         </p>
 
