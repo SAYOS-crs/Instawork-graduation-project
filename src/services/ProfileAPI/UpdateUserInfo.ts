39d5bf39b73003b15d9c55dccd2cf58656604e58
Eslam Elawdy <eslam.mohamed.ki123@gmail.com>
2025-12-07 00:13:54 +0200

    HotFix(Vercel)

--- src/services/ProfileAPI/UpdateUserInfo.ts
+++ src/services/ProfileAPI/UpdateUserInfo.ts
@@ -9,25 +9,34 @@ export default async function UpdateingUserInfo(UpdatedData : FormData) {
 
     const token = await GetToken()
 
+    if (token) {
 
-    try {
-        const req = await fetch( `https://gp2025.runasp.net/api/v1/users`,{
-        method : 'PATCH',
-        headers : {
-            token : token+""
-        },
-        body : UpdatedData
-    } )
-    if (req.ok) {
-        const respons = await req.json()
         
-        return await respons
+        try {
+            const req = await fetch( `https://gp2025.runasp.net/api/v1/users`,{
+            method : 'PATCH',
+            headers : {
+                token : token+""
+            },
+            body : UpdatedData
+        } )
+        if (req.ok) {
+            const respons = await req.json()
+            
+            return await respons
+        }
+        
+        return req
+        } catch (error) {
+            return error
+        }
+
+
+
+
     }
-    
-    return req
-    } catch (error) {
-        return error
-    }
+
+
     
 }
 
