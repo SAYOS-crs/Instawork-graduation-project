e1cb30589165d809a603dcc4cdecab595b8b5c3b
Eslam Elawdy <eslam.mohamed.ki123@gmail.com>
2025-12-28 03:34:10 +0200

    warning(Warning-Sign)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,6 @@ export default async function RootLayout({
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Providers>
             <Navbar />
-
             {children}
             <Footer />
           </Providers>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
-// import { useTranslations } from "next-intl";
-// import LanguageSwitcher from "./language-switcher";
+import { useTranslations } from "next-intl";
+import LanguageSwitcher from "./language-switcher";
+import { BiError } from "react-icons/bi";
 import {
   Navbar as NavbarComponent,
   NavbarBrand,
@@ -12,6 +13,7 @@ import {
   NavbarMenuItem,
   Button,
 } from "@heroui/react";
+import { FaRegHandshake } from "react-icons/fa6";
 import Instawork_icon from "@/../public/Icon/Instawork-logo-white.png";
 
 import Link from "next/link";
@@ -21,120 +23,130 @@ import Image from "next/image";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  // const t = useTranslations("NavBar");
+  const t = useTranslations("NavBar");
   const path = usePathname();
   const { status, data } = useSession();
   const session = useSession();
 
   const NavItems = [
-    { lable: "خدمات", href: "/services" },
-    { lable: "الأعمال", href: "/Jobs" },
-    { lable: "لوحه التحكم ", href: "/Dashbord/Profile" },
+    { lable: t("services_lab"), href: "/services" },
+    { lable: t("jobs_lab"), href: "/Jobs" },
+    { lable: t("dashbord_lab"), href: "/Dashbord/Profile" },
   ];
 
   return (
-    <NavbarComponent
-      className="backgroundNavBar py-2   "
-      onMenuOpenChange={setIsMenuOpen}
-    >
-      <NavbarContent className="">
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden "
-        />
-        <NavbarBrand
-          className={`${path === "/" && "text-white"} max-sm:hidden `}
-        >
-          {/* <FaRegHandshake className="text-4xl" /> */}
-          <Link href={"/"} className="font-bold text-inherit text-xl  ps-3">
-            <Image
-              className="mx-auto "
-              src={Instawork_icon}
-              width={60}
-              height={60}
-              alt="s"
-            />
-          </Link>
-        </NavbarBrand>
-      </NavbarContent>
-
-      <NavbarContent
-        className="hidden sm:flex gap-4 relative "
-        justify="center"
+    <>
+      <NavbarComponent
+        className="backgroundNavBar py-2   "
+        onMenuOpenChange={setIsMenuOpen}
       >
-        {NavItems.map((item, index) => (
-          <NavbarItem
-            key={`${item}-${index}`}
-            className="custom-active-class text-lg font-semibold mx-3 text-primry-background hover:text-white transition-all duration-300"
-            isActive={item.href === path}
+        <NavbarContent className="">
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className="sm:hidden "
+          />
+          <NavbarBrand
+            className={`${path === "/" && "text-white"} max-sm:hidden `}
           >
-            <Link className="relative" color="foreground" href={item.href}>
-              {item.lable}
+            {/* <FaRegHandshake className="text-4xl" /> */}
+            <Link href={"/"} className="font-bold text-inherit text-xl  ps-3">
+              <Image
+                className="mx-auto "
+                src={Instawork_icon}
+                width={60}
+                height={60}
+                alt="s"
+              />
             </Link>
-          </NavbarItem>
-        ))}
-        <div>{/* <LanguageSwitcher /> */}</div>
-      </NavbarContent>
+          </NavbarBrand>
+        </NavbarContent>
 
-      <NavbarContent justify="end">
-        <NavbarItem className="flex ">
-          {status === "authenticated" ? (
-            <Button
-              onPress={() => signOut({ callbackUrl: "/" })}
-              className="hover:bg-primry-background hover:text-main-background transition-all duration-300 py-2 px-6 min-w-24 m-2 rounded-lg text-center bg-main-background text-primry-background border-2 border-primry-background font-semibold shadow-lg hover:shadow-xl"
-            >
-              {"تسجيل الخروج"}
-            </Button>
-          ) : (
-            <>
-              <Link
-                className="hover:bg-primry-background hover:text-main-background transition-all duration-300 py-2 px-6 min-w-24 m-2 rounded-lg text-center bg-main-background text-primry-background border-2 border-primry-background font-semibold shadow-lg hover:shadow-xl"
-                href={"/Register"}
-              >
-                {"انشئ حساب"}
-              </Link>
-
-              <Link
-                className="hover:bg-main-background hover:text-primry-background transition-all duration-300 py-2 px-6 min-w-24 m-2 rounded-lg text-center bg-primry-background text-main-background border-2 border-primry-background font-semibold shadow-lg hover:shadow-xl"
-                href={"/Login"}
-              >
-                {"تسجيل الدخول"}
-              </Link>
-            </>
-          )}
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarMenu className="py-10">
-        <Link
-          className="w-full p-5 hover:bg-main-background rounded-md"
-          href={"/"}
+        <NavbarContent
+          className="hidden sm:flex gap-4 relative "
+          justify="center"
         >
-          Home
-        </Link>
-
-        {NavItems.map((item, index) => (
-          <NavbarMenuItem
-            className="p-5 hover:bg-main-background rounded-md"
-            key={`${item}-${index}`}
-          >
-            <Link
-              className="w-full"
-              color={
-                index === 2
-                  ? "primary"
-                  : index === NavItems.length - 1
-                  ? "danger"
-                  : "foreground"
-              }
-              href={item.href}
+          {NavItems.map((item, index) => (
+            <NavbarItem
+              key={`${item}-${index}`}
+              className="custom-active-class text-lg font-semibold mx-3 text-primry-background hover:text-white transition-all duration-300"
+              isActive={item.href === path}
             >
-              {item.lable}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-        {/* <LanguageSwitcher /> */}
-      </NavbarMenu>
-    </NavbarComponent>
+              <Link className="relative" color="foreground" href={item.href}>
+                {item.lable}
+              </Link>
+            </NavbarItem>
+          ))}
+          <div>
+            <LanguageSwitcher />
+          </div>
+        </NavbarContent>
+
+        <NavbarContent justify="end">
+          <NavbarItem className="flex ">
+            {status === "authenticated" ? (
+              <Button
+                onPress={() => signOut({ callbackUrl: "/" })}
+                className="hover:bg-primry-background hover:text-main-background transition-all duration-300 py-2 px-6 min-w-24 m-2 rounded-lg text-center bg-main-background text-primry-background border-2 border-primry-background font-semibold shadow-lg hover:shadow-xl"
+              >
+                {t("logout")}
+              </Button>
+            ) : (
+              <>
+                <Link
+                  className="hover:bg-primry-background hover:text-main-background transition-all duration-300 py-2 px-6 min-w-24 m-2 rounded-lg text-center bg-main-background text-primry-background border-2 border-primry-background font-semibold shadow-lg hover:shadow-xl"
+                  href={"/Register"}
+                >
+                  {t("signup")}
+                </Link>
+
+                <Link
+                  className="hover:bg-main-background hover:text-primry-background transition-all duration-300 py-2 px-6 min-w-24 m-2 rounded-lg text-center bg-primry-background text-main-background border-2 border-primry-background font-semibold shadow-lg hover:shadow-xl"
+                  href={"/Login"}
+                >
+                  {t("login")}
+                </Link>
+              </>
+            )}
+          </NavbarItem>
+        </NavbarContent>
+
+        <NavbarMenu className="py-10">
+          <Link
+            className="w-full p-5 hover:bg-main-background rounded-md"
+            href={"/"}
+          >
+            Home
+          </Link>
+
+          {NavItems.map((item, index) => (
+            <NavbarMenuItem
+              className="p-5 hover:bg-main-background rounded-md"
+              key={`${item}-${index}`}
+            >
+              <Link
+                className="w-full"
+                color={
+                  index === 2
+                    ? "primary"
+                    : index === NavItems.length - 1
+                    ? "danger"
+                    : "foreground"
+                }
+                href={item.href}
+              >
+                {item.lable}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+          <LanguageSwitcher />
+        </NavbarMenu>
+      </NavbarComponent>
+      <div className="fixed  z-50  w-full h-8 bg-red-700 text-white text-center py-1">
+        <BiError className="inline-block size-5 mx-2" />
+        if the Routing not working try to change the language
+        <BiError className="inline-block size-5 mx-2" />
+        <span className="text-md bg-red-700 py-2 block">production bug</span>
+      </div>
+    </>
   );
 }
